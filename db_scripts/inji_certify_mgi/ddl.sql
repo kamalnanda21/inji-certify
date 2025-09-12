c47f44682cc15200ffa1899f6b5a857f4e7e00db
Kamal . <kamal.1@in.ey.com>
2025-09-12 11:34:28 +0530

    added folders for mgi and mda for db initialisation

---
+++ db_scripts/inji_certify_mgi/ddl.sql
@@ -0,0 +1,9 @@
+\c inji_certify_mgi
+
+\ir ddl/certify-key_alias.sql
+\ir ddl/certify-key_policy_def.sql
+\ir ddl/certify-key_store.sql
+\ir ddl/certify-ca_cert_store.sql
+\ir ddl/certify-rendering_template.sql
+\ir ddl/certify-credential_config.sql
+
